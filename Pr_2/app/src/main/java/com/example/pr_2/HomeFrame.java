e261ef879a33a1f66cecf2e855ce3fe42d24f381
YuliyaKiryanova <72200236+KiryanovaJulia@users.noreply.github.com>
2023-02-25 02:08:20 +0300

    pr_2_new_version

--- Pr_2/app/src/main/java/com/example/pr_2/HomeFrame.java
+++ Pr_2/app/src/main/java/com/example/pr_2/HomeFrame.java
@@ -1,6 +1,8 @@
 package com.example.pr_2;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,6 +12,13 @@ public class HomeFrame extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_frame);
+
+    }
+
+    public void buttonClick(View view)
+    {
+        Intent myIntent = new Intent(HomeFrame.this, MainActivity.class);
+        HomeFrame.this.startActivity(myIntent);
 
     }
 }
--- Pr_2/app/src/main/java/com/example/pr_2/MainActivity.java
+++ Pr_2/app/src/main/java/com/example/pr_2/MainActivity.java
@@ -2,14 +2,16 @@ package com.example.pr_2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
 import android.content.Intent;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "myLogs";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,11 +22,14 @@ public class MainActivity extends AppCompatActivity {
 
     public void buttonClick(View view)
     {
-        view.findViewById(R.id.sign_in).setOnClickListener(v -> {
-            Intent intent = new Intent(MainActivity.this, HomeFrame.class);
-            startActivity(intent);
-        });
-
+        final EditText login = (EditText)findViewById(R.id.log);
+        final EditText password = (EditText)findViewById(R.id.passw);
+        if (login.getText().toString().equals("admin") && password.getText().toString().equals("admin"))
+            {
+                Intent myIntent = new Intent(MainActivity.this, HomeFrame.class);
+                MainActivity.this.startActivity(myIntent);
+            }
+        Log.d(TAG,"Кнопка нажата");
     }
 
 }
