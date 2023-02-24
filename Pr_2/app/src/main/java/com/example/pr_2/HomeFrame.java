e56097f110ea6571b36e23fda6eb864d0c27c980
YuliyaKiryanova <72200236+KiryanovaJulia@users.noreply.github.com>
2023-02-24 23:46:37 +0300

    practica 2

---
+++ Pr_2/app/src/main/java/com/example/pr_2/HomeFrame.java
@@ -0,0 +1,15 @@
+package com.example.pr_2;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HomeFrame extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_frame);
+
+    }
+}
